30b21f6901802bbdabe0ad38f4f6c0b39ac4dcca
Prokop Randáček <prokop@randacek.dev>
2021-03-27 10:20:55 +0100

    api light and time

--- src/sceneapi.h
+++ src/sceneapi.h
@@ -1,16 +1,19 @@
 // vim: filetype=c
 #include "vector.h"
 
-// private
+// === private ===
 int nextShpIndex(void);
 int nextGrpIndex(void);
 ShapeType getShapeType(int i);
 
-// public
+// === public ===
+
+// shape editing
 void setShapeClr(int i, vec3 value);
 void setShapePos(int i, vec3  value);
 void setShapeRv (int i, float value);
 
+// shape creating
 int createSphere  (vec3 pos,   vec3 clr, float rv, float r);
 int createCube    (vec3 pos,   vec3 clr, float rv, vec3 scale);
 int createTorus   (vec3 pos,   vec3 clr, float rv, float r, float fatness);
@@ -18,4 +21,9 @@ int createCapsule (vec3 start, vec3 clr, float rv, vec3 end, float r);
 int createCylinder(vec3 start, vec3 clr, float rv, vec3 end, float r);
 int createCCone   (vec3 start, vec3 clr, float rv, vec3 end, float startR, float endR);
 
+// group creating
 int createGroup(int a, int b, OperationType op, float k);
+
+// other
+void moveLight(vec3 pos);
+float getTime(void);
--- src/settings.h
+++ src/settings.h
@@ -8,11 +8,11 @@
 #define MOUSE_SENS 0.12f
 #define MOVE_SPEED 5.0f
 
-// this does not do anything rn
-#define WANTED_FPS 60
-
 #define SHAPE_NUM 4
 #define GROUP_NUM 3
+
+// this does not do anything rn
+#define WANTED_FPS 60
 
 // internal. Don't change these
 #define SHAPE_SIZE 16
