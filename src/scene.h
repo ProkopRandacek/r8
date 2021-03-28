ff15d3613030f2b3180f25c6d64194de349a656e
Prokop Randáček <prokop@randacek.dev>
2021-03-28 17:44:17 +0200

    scene transport speedup

--- src/scene.h
+++ src/scene.h
@@ -1,12 +1,9 @@
 // vim: filetype=c
 #include "camera.h"
 
-
 void createScene(void);
 void updateScene(void);
-void createCamera(void);
 void sendCamera(void);
-void createLight(void);
 void createObjects(void);
 void sendObjects(void);
 void freeObjects(void);
