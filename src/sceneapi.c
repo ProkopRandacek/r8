95fc9e9d2b4ca6debe5d582bfda52bc8a8de6d60
Prokop Randáček <prokop@randacek.dev>
2021-03-28 17:44:17 +0200

    scene transport speedup

--- src/sceneapi.c
+++ src/sceneapi.c
@@ -12,7 +12,7 @@ int freeGrpIndex = 0;
 extern Shape*      shapes[MAX_SHAPE_NUM];
 extern ShapeGroup* groups[MAX_GROUP_NUM];
 extern GL* gl;
-extern int shapeNum, groupNum;
+extern int shapeNum, groupNum, sceneChanged;
 
 // ===== PRIVATE =====
 
@@ -42,45 +42,45 @@ inline ShapeType getShapeType(int i) { return shapes[i]->type; }
 
 // Shape editing. `i` is the shape ID
 
-void setShapeClr(int i, vec3  value) { shapes[i]->clr = value; }
-void setShapePos(int i, vec3  value) { shapes[i]->pos = value; }
-void setShapeRv (int i, float value) { shapes[i]->rv  = value; }
+void setShapeClr(int i, vec3  value) { shapes[i]->clr = value; sceneChanged = 1; }
+void setShapePos(int i, vec3  value) { shapes[i]->pos = value; sceneChanged = 1; }
+void setShapeRv (int i, float value) { shapes[i]->rv  = value; sceneChanged = 1; }
 
 // Shape creating. Returns ID of the shape. If ID == -1 then you run out of space for shapes.
 
 int createSphere(vec3 pos, vec3 clr, float rv, float r) {
 	int i = nextShpIndex();
-	if (i >= 0) { shapes[i] = sph(pos, clr, r, rv); }
+	if (i >= 0) { shapes[i] = sph(pos, clr, r, rv); sceneChanged = 1; }
 	return i;
 }
 
 int createCube(vec3 pos, vec3 clr, float rv, vec3 scale) {
 	int i = nextShpIndex();
-	if (i >= 0) { shapes[i] = cube(pos, clr, scale, 0.0f, rv); }
+	if (i >= 0) { shapes[i] = cube(pos, clr, scale, 0.0f, rv); sceneChanged = 1; }
 	return i;
 }
 
 int createTorus(vec3 pos, vec3 clr, float rv, float r, float fatness) {
 	int i = nextShpIndex();
-	if (i >= 0) { shapes[i] = tor(pos, clr, r, fatness, rv); }
+	if (i >= 0) { shapes[i] = tor(pos, clr, r, fatness, rv); sceneChanged = 1; }
 	return i;
 }
 
 int createCapsule(vec3 start, vec3 clr, float rv, vec3 end, float r) {
 	int i = nextShpIndex();
-	if (i >= 0) { shapes[i] = caps(start, end, clr, r, rv); }
+	if (i >= 0) { shapes[i] = caps(start, end, clr, r, rv); sceneChanged = 1; }
 	return i;
 }
 
 int createCylinder(vec3 start, vec3 clr, float rv, vec3 end, float r) {
 	int i = nextShpIndex();
-	if (i >= 0 ) { shapes[i] = cyl(start, end, clr, r, rv); }
+	if (i >= 0 ) { shapes[i] = cyl(start, end, clr, r, rv); sceneChanged = 1; }
 	return i;
 }
 
 int createCCone(vec3 start, vec3 clr, float rv, vec3 end, float startR, float endR) {
 	int i = nextShpIndex();
-	if (i >= 0 ) { shapes[i] = ccone(start, end, clr, startR, endR, rv); }
+	if (i >= 0 ) { shapes[i] = ccone(start, end, clr, startR, endR, rv); sceneChanged = 1; }
 	return i;
 }
 
@@ -90,6 +90,7 @@ int createGroup(int a, int b, OperationType op, float k) {
 	if (i == -1) { return -1; }
 	else {
 		groups[i] = group(a, b, op, k);
+		sceneChanged = 1;
 
 		return i + MAX_SHAPE_NUM; // external group IDs start on MAX_SHAPE_NUM -> if ID is bigger than MAX_SHAPE_NUM is a group, otherwise its a shape
 	}
@@ -98,6 +99,7 @@ int createGroup(int a, int b, OperationType op, float k) {
 void moveLight(vec3 pos) {
 	float f[] = {pos.x, pos.y, pos.z};
 	shdSetVec3Array(gl->s, "lightPos", 1, f);
+	sceneChanged = 1;
 }
 
 float getTime() {
