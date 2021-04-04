4e256f9b4a6376f56c930ae22d58cfe7b0693357
Prokop Randáček <prokop@randacek.dev>
2021-04-04 11:31:41 +0200

    make it toggle

--- src/opengl.c
+++ src/opengl.c
@@ -113,10 +113,12 @@ void initOGL(Scene s) {
 
 	glfwSetInputMode(gl->window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-	/*glEnable(GL_DEBUG_OUTPUT);
+	#ifdef DO_GL_DEBUG
+	glEnable(GL_DEBUG_OUTPUT);
 	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
 	glDebugMessageCallback(glDebugOutput, NULL);
-	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);*/
+	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);
+	#endif
 
 	dprint("GL DONE");
 }
