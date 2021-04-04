0df5c98c9ea719ad6c28028341abbba9ec771a6d
Prokop Randáček <prokop@randacek.dev>
2021-04-04 11:22:11 +0200

    gl debug causes SIGSEGV when not supported

--- src/opengl.c
+++ src/opengl.c
@@ -113,10 +113,10 @@ void initOGL(Scene s) {
 
 	glfwSetInputMode(gl->window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-	glEnable(GL_DEBUG_OUTPUT);
+	/*glEnable(GL_DEBUG_OUTPUT);
 	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
 	glDebugMessageCallback(glDebugOutput, NULL);
-	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);
+	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);*/
 
 	dprint("GL DONE");
 }
