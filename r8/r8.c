42a230c9a637927659f945467001cc2e4c0a8d4c
Prokop Randacek <prokop@randacek.dev>
2021-07-13 16:25:31 +0200

    redirect raylib output to libucw output

--- r8/r8.c
+++ r8/r8.c
@@ -7,9 +7,23 @@
 #include "common.h"
 #include "autoconf.h"
 
+void raylib_log_to_libucw_log(int log_level, const char *text, va_list args);
+void raylib_log_to_libucw_log(int raylib_log_level, const char *text, va_list args) {
+	int libucw_log_level;
+	switch (raylib_log_level) {
+		case LOG_DEBUG:   libucw_log_level = L_DEBUG; break;
+		case LOG_INFO:    libucw_log_level = L_INFO;  break;
+		case LOG_WARNING: libucw_log_level = L_WARN;  break;
+		case LOG_ERROR:   libucw_log_level = L_ERROR; break;
+	}
+	msg(libucw_log_level, text, args);
+}
+
 int main(int argc, char* argv[]) {
 	log_init(argv[0]);
 	msg(L_INFO, "start");
+
+	SetTraceLogCallback(raylib_log_to_libucw_log);
 
 	const int width = 800;
 	const int height = 450;
@@ -29,3 +43,4 @@ int main(int argc, char* argv[]) {
 
 	return 0;
 }
+
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ Rewrite from scratch wip
 
 ## Building
 R8 is build using the [libucw](http://www.ucw.cz/libucw/)'s build system so you need it installed to build R8. You can clone, compile and install it manually, or run the [`getucw`](./getucw) script to do it automatically.  
-R8 also requires raylib. Ideally, you would install raylib with `yay -S raylib`. Alternatively you can compile/download and install raylib from source manually or automatically using the [`getraylib`](./getraylib) script.  
+R8 also requires raylib. Ideally, you would install raylib with `yay -S raylib`. Alternatively you can compile/download and install raylib from source manually.  
 
 ### Build dependencies
 - make
